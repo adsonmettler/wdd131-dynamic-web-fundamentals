e52eb2b138dd42034a90d2d170fa7e5acfeccebb
adsonmettler <adsonmettler@gmail.com>
2024-04-25 13:48:07 -0400

    Sctructure for HomePage - updated 2024-04-25

---
+++ test.js
@@ -0,0 +1,12 @@
+
+let names = ["Bob", "Sue", "Jorge", "Svetlana"];
+
+names.push("Grace");
+console.log(names);
+
+names.pop()
+console.log(names);
+
+let mystring = "This is my string!";
+mystring.length;
+console.log(mystring.length);
